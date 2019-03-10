43228234cdef881be7d0bdfc88467aafe4da7810
Joshua <olajidejoshua4real@gmail.com>
2019-03-09 18:22:08 -0800

    Instruction.php added to the working directory

---
+++ instruction.php
@@ -0,0 +1,51 @@
+<?php include ('includes/header.php'); ?>
+<body class="index_body">
+	<div class="container ">
+		<div class="row">
+			<div class="col-lg-3 col-md-6 col-sm-12">
+				<div class="card mt-5" >
+					<img src="images/Opi51c74d0125fd4.png" class="card-img-top" alt="Steve Jobs">
+					<div class="card-body">
+						<div class="card-title text-center text-muted badge badge-light" style="width: 100%;"><h4>STEVE JOBS</h4></div>
+							<p>Phone No: +1118e9</p>
+							<p>Reg No: 71hgfbdg</p>
+							<p>Dept: Compt.Sci</p>
+							<p>Level: 1000L</p>
+						
+					</div>
+					<div class="card-footer">
+						<button type="submit" class="btn btn-grey"> LOG OUT   <i class="fa fa-sign-out"></i></button>
+					</div>
+				</div>
+			</div>
+			<div class="col-lg-9 col-sm-12 take-in"> 
+				<div class="custom mt-5 hide">
+				
+					<h2 class="" id="list-subject">Please follow the Instructions Giving below before proceeding.....</h2>
+					<hr>
+					<ol class="mt-5 ">
+						<li>Instruction A Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</li>
+						<li>Instruction A Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</li>
+						<li>Instruction A Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</li>
+						<li>Instruction A Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</ol>
+						
+					</ul>
+					<a href="javascript:void()" id="index_proceed"><button class="btn btn-info" style="width: 100%;">PROCEED &nbsp;&nbsp;&nbsp;<i class="fa fa-arrow-right"></i></button></a>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+
+<?php include ('includes/footer.php'); ?>
+
